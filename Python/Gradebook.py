9181c6d476d32d59a5f175aa6d0c9dbfe45d1384
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-05 21:40:20 -0600

    Create Gradebook.py

---
+++ Python/Gradebook.py
@@ -0,0 +1,15 @@
+# Last semester gradebook
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+gradebook[-1][-1] = 98
+gradebook[2].remove(85)
+gradebook[2].append("Pass")
+print(gradebook)
+
+# Combine last semester and current gradebooks
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
