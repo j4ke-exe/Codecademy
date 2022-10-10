3b94f61299599cdd10cc40e4c9e94bd63703995e
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-10 13:57:49 -0600

    Create Rounding.py

---
+++ Python/Rounding.py
@@ -0,0 +1,16 @@
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Get max price
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+print(max_price)
+
+# Get min price
+min_price = min(tshirt_price, shorts_price, mug_price, poster_price)
+print(min_price)
+
+# Round price of tshirt by 1 decimal place
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
