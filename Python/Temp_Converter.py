1f51cbc6018a0b37bb8157d55d18c851af4e6380
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-11 12:29:07 -0600

    Create Temp_Converter.py

---
+++ Python/Temp_Converter.py
@@ -0,0 +1,15 @@
+# Convert fahrenheit to celsius
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+f100_in_celsius = f_to_c(32)
+c = round(f100_in_celsius)
+print("Temp in Celsius:", c)
+
+# Convert celsius to fahrenheit
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+c0_in_fahrenheit = c_to_f(32)
+f = round(c0_in_fahrenheit)
+print("Temp in Fahrenheit:", f)
