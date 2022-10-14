be4d8b3cb6f08b93fdb59d3a4e6415d50efb9880
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-13 21:13:34 -0600

    Update Challenge_Advanced_List.py

--- Python/Challenge_Advanced_List.py
+++ Python/Challenge_Advanced_List.py
@@ -61,7 +61,7 @@ def double_index(lst, index):
         new_lst = lst[0:index]
     # Adds double the value at index to the new list 
     new_lst.append(lst[index]*2)
-    #  Adds the rest of the original list
+    # Adds the rest of the original list
     new_lst = new_lst + lst[index+1:]
     return new_lst
 print(double_index([3, 8, -10, 12], 2))
