9040f05c534a3067af76ef99f7fc72303a17785c
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-15 11:49:14 -0600

    Update Challenge_Loop.py

--- Python/Challenge_Loop.py
+++ Python/Challenge_Loop.py
@@ -13,5 +13,23 @@ print(divisible_by_ten([20, 25, 30, 35, 40]))
 
 # Output = 3
 
-#-----------------------------------------------------
+#--------------------------------------------------------
 
+# Question:
+# Create a function named add_greetings() which takes a list of strings named names as a parameter.
+# In the function, create an empty list that will contain each greeting. Add the string 'Hello, ' in front of each name in names and append the greeting to the list.
+# Return the new list containing the greetings.
+
+# Solution:
+def add_greetings(names):
+   new_list = []
+   for n in names:
+      new_list.append("Hello, " + n)
+   return new_list
+print(add_greetings(["Owen", "Max", "Sophie"]))
+
+# Output = ['Hello, Owen', 'Hello, Max', 'Hello, Sophie']
+
+#--------------------------------------------------------
+
+# Question:
