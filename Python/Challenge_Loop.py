ccc30b707efbd0cb87719a9360945c371e497a9a
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-16 09:27:14 -0600

    Update Challenge_Loop.py

--- Python/Challenge_Loop.py
+++ Python/Challenge_Loop.py
@@ -49,3 +49,18 @@ print(delete_starting_evens([4, 8, 10]))
 #--------------------------------------------------------
 
 # Question:
+# Create a function named odd_indices() that has one parameter named lst.
+# The function should create a new empty list and add every element from lst that has an odd index. The function should then return this new list.
+# For example, odd_indices([4, 3, 7, 10, 11, -2]) should return the list [3, 10, -2].
+
+# Solution:
+def odd_indices(lst):
+   new_list = []
+   for index in range(1, len(lst), 2):
+      new_list.append(lst[index])
+   return new_list
+print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+#--------------------------------------------------------
+
+# Question:
