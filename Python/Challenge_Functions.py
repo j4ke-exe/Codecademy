483463a2e16ceddec0ef3a339cd8843213af5c0e
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-17 13:24:22 -0600

    Create Challenge_Functions.py

---
+++ Python/Challenge_Functions.py
@@ -0,0 +1,27 @@
+# Question:
+# Write a function named tenth_power() that has one parameter named num.
+# The function should return num raised to the 10th power.
+
+# Solution:
+def tenth_power(num):
+  return num ** 10
+# 1 to the 10th power is 1
+print(tenth_power(1))
+# 0 to the 10th power is 0
+print(tenth_power(0))
+# 2 to the 10th power is 1024
+print(tenth_power(2))
+
+#-----------------------------------------------------------
+
+# Question:
+# Write a function named square_root() that has one parameter named num.
+# Use exponents (**) to return the square root of num.
+
+# Solution:
+def square_root(num):
+  return num ** .5
+# should print 4
+print(square_root(16))
+# should print 10
+print(square_root(100))
