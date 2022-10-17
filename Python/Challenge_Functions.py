f6f9943dcdd6d9e9f4d31789767de5195f5a9766
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-17 14:37:02 -0600

    Update Challenge_Functions.py

--- Python/Challenge_Functions.py
+++ Python/Challenge_Functions.py
@@ -29,3 +29,45 @@ print(square_root(16))
 print(square_root(100))
 
 # Output = 4.0, 10.0
+
+#-----------------------------------------------------------
+
+# Question:
+# Create a function called win_percentage() that takes two parameters named wins and losses.
+# This function should return out the total percentage of games won by a team based on these two numbers.
+
+# Solution:
+def win_percentage(wins, losses):
+  return wins / (wins + losses) * 100
+# Prints 50
+print(win_percentage(5, 5))
+# Prints 100
+print(win_percentage(10, 0))
+
+#-----------------------------------------------------------
+
+# Question:
+# Write a function named average() that has two parameters named num1 and num2.
+# The function should return the average of these two numbers.
+
+# Solution:
+def average(num1, num2):
+  return (num1 + num2) / 2
+# The average of 1 and 100 is 50.5
+print(average(1, 100))
+# The average of 1 and -1 is 0
+print(average(1, -1))
+
+#-----------------------------------------------------------
+
+# Question:
+# Write a function named remainder() that has two parameters named num1 and num2.
+# The function should return the remainder of twice num1 divided by half of num2.
+
+# Solution:
+def remainder(num1, num2):
+  return (num1 * 2) % (num2 / 2)
+# Prints 2.0
+print(remainder(15, 14))
+# Prints 0.0
+print(remainder(9, 6))
