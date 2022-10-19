2ffd46f6c18da768c2d865f4c49195ceaf1bf011
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-18 20:58:12 -0600

    Update Concatenate.py

--- Python/Concatenate.py
+++ Python/Concatenate.py
@@ -1,11 +1,24 @@
+# Concatenate first 3 letters of each name
 first_name = "Jake"
 last_name = "Garrison"
 
 def account_generator(first_name, last_name):
-  # Concatenate first 3 letters of each name
   account_name = first_name[:3] + last_name[:3]
   return account_name
 
 new_account = account_generator(first_name, last_name)
 
 print(new_account)
+
+#-----------------------------------------------------
+
+# Concatenate last 3 letters of each name
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  return first_name[-3:] + last_name[-3:]
+
+temp_password = password_generator(first_name, last_name)
+
+print(temp_password)
