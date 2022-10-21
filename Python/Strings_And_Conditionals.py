6c84a28fef98974070ad5eb73b9d5280c3716d36
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-21 00:09:41 -0600

    Update Strings_And_Conditionals.py

--- Python/Strings_And_Conditionals.py
+++ Python/Strings_And_Conditionals.py
@@ -3,3 +3,13 @@ def letter_check(word, letter):
     if character == letter:
       return True
   return False
+
+def contains(big_string, little_string):
+  return little_string in big_string
+
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
