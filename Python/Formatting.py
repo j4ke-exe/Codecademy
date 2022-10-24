99d25b362b70978661a0c75a82993b4228856742
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-24 10:56:53 -0600

    Create Formatting.py

---
+++ Python/Formatting.py
@@ -0,0 +1,14 @@
+poem_title = "spring storm"
+poem_author = "William Carlos Williams"
+
+# Returns the string in title case
+poem_title_fixed = poem_title.title()
+print(poem_title_fixed)
+
+# Returns the string with all uppercase
+poem_author_fixed_upper = poem_author.upper()
+print(poem_author_fixed_upper)
+
+# Returns the string with all lowercase
+poem_author_fixed_lower = power_author.lower()
+print(poem_author_fixed_lower)
