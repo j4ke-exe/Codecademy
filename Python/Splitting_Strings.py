277e36595cd3b7e9022fbc79383624d919d153b5
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-24 18:30:59 -0600

    Update Splitting_Strings.py

--- Python/Splitting_Strings.py
+++ Python/Splitting_Strings.py
@@ -10,3 +10,14 @@ for name in author_names:
   # Split [-1] = first name and append only last name
   author_last_names.append(name.split()[-1])
 print(author_last_names)
+
+#----------------------------------------------------
+
+# Splitting and joining lines
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+love_maybe_lines_stripped = []
+for line in love_maybe_lines:
+  love_maybe_lines_stripped.append(line.strip())
+love_maybe_full = '\n'.join(love_maybe_lines_stripped)
+print(love_maybe_full)
