c8ef1c16ebcdec0a5d5fc36e63e3093759adae84
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-24 18:33:43 -0600

    Update Splitting_Strings.py

--- Python/Splitting_Strings.py
+++ Python/Splitting_Strings.py
@@ -21,3 +21,14 @@ for line in love_maybe_lines:
   love_maybe_lines_stripped.append(line.strip())
 love_maybe_full = '\n'.join(love_maybe_lines_stripped)
 print(love_maybe_full)
+
+#----------------------------------------------------
+
+# Replace strings
+toomer_bio = \
+"""
+Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career, was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman, born into slavery in 1839 in Chatham County, North Carolina. Jean Tomer is most well known for his first book Cane, which vividly portrays the life of African-Americans in southern farmlands.
+"""
+
+toomer_bio_fixed = toomer_bio.replace('Tomer', 'Toomer')
+print(toomer_bio_fixed)
