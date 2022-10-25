f666cdef16b9441407ce7c10d63e79fdc2d51ef5
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-24 18:57:47 -0600

    Update Format_Strings.py

--- Python/Format_Strings.py
+++ Python/Format_Strings.py
@@ -1,3 +1,16 @@
 def book_title_card(title, author):
   return "The book \"{}\" is written by {}.".format(title, author)
 print(book_title_card("Dark Tower", "Stephen King"))
+
+#-----------------------------------------------------------------
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {title} by {author} was originally  published in {original_work} in {publishing_date}.".format(publishing_date=publishing_date, author=author, title=title, original_work=original_work)
+  return poem_desc
+
+author = "Shel Silverstein"
+title = "My Beard"
+original_work = "Where the Sidewalk Ends"
+publishing_date = "1974"
+
+my_beard_description = poem_description(publishing_date, author, title, original_work)
+print(my_beard_description) 
