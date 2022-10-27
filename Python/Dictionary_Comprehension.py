4b7c99e13a5286c98c5fdce53177b7561f34e31a
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-27 17:31:43 -0600

    Update Dictionary_Comprehension.py

--- Python/Dictionary_Comprehension.py
+++ Python/Dictionary_Comprehension.py
@@ -8,3 +8,18 @@ zipped_drinks = zip(drinks, caffeine)
 drinks_to_caffeine = {key: value for key, value in zipped_drinks}
 
 print(drinks_to_caffeine)
+
+# ----------------------------------------------------------------
+
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {key: value for key, value in zip(songs, playcounts)}
+
+print(plays)
+
+plays["Purple Haze"] = 1
+plays["Respect"] = 94
+
+library = {"The Best Songs": plays, "Sunday Feelings": {}}
+print(library)
