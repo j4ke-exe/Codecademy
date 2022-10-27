54ef07c8c0e95dd2531a554a30a4f80fa1dac316
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-27 16:00:54 -0600

    Update Plot.py

--- Python/Plot.py
+++ Python/Plot.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 # Set range 1 - 12
 numbers_a = range(1, 13)
 
-# Sample 12 from a range from 1 - 999
+# Sample 12 from a range of 1 - 999
 numbers_b = random.sample(range(1000), 12)
 
 # Plot both variables
