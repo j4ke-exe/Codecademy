d822b518d1c6e78a4253dcbe184b02ae4ea977b0
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-27 16:06:23 -0600

    Create Decimal_Points.py

---
+++ Python/Decimal_Points.py
@@ -0,0 +1,9 @@
+# Import Decimal
+from decimal import Decimal
+
+# Fix the floating point math
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
