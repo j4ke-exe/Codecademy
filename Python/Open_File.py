d4da9edf1f4de6589d78400536614891b1e27a34
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-27 22:28:17 -0600

    Update Open_File.py

--- Python/Open_File.py
+++ Python/Open_File.py
@@ -1,3 +1,10 @@
 with open('welcome.txt') as text_file:
   text_data = text_file.read()
 print(text_data)
+
+#-------------------------------------
+
+# Iterate through each line and assign it to a variable.
+with open('how_many_lines.txt') as lines_doc:
+  for line in lines_doc.readlines():
+    print(line)
