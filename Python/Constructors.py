06a60d111ec2b63317155c7db2c1b94565b96f97
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-28 20:35:32 -0600

    Update Constructors.py

--- Python/Constructors.py
+++ Python/Constructors.py
@@ -6,3 +6,14 @@ class Circle:
     print("New circle with diameter: {}".format(diameter))
 
 teach_table = Circle(36)
+
+#---------------------------------------------------------
+
+class Store:
+  pass
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
