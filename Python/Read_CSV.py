ff024b0b84e07fe709dcceb5ff3ea7c18b37f591
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-28 19:38:50 -0600

    Update Read_CSV.py

--- Python/Read_CSV.py
+++ Python/Read_CSV.py
@@ -47,3 +47,23 @@ import json
 
 with open('data.json', 'w') as data_json:
   json.dump(data_payload, data_json)
+  
+#----------------------------------------------
+
+import csv
+
+compromised_users = []
+
+with open('passwords.csv') as password:
+  contents = password.read()
+print(contents)
+
+
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  
+  for password_row in password_csv:
+    #print(password_row['Username'])
+    compromised_users.append(password_row['Username'])
+
+print(compromised_users)
