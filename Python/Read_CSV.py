7ba6e6e5c4704a46784302afba769032afefea7d
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-28 19:20:13 -0600

    Update Read_CSV.py

--- Python/Read_CSV.py
+++ Python/Read_CSV.py
@@ -5,3 +5,12 @@ with open('cool_csv.csv') as cool_csv_file:
   cool_csv_dict = csv.DictReader(cool_csv_file)
   for row in cool_csv_dict:
     print(row["Cool Fact"])
+
+#----------------------------------------------
+
+import csv
+
+with open('books.csv') as books_csv:
+  books_reader = csv.DictReader(books_csv, delimiter='@')
+  isbn_list = [book['ISBN'] for book in books_reader]
+print(isbn_list)
