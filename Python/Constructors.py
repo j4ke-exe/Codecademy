dfb341c47019b419444654783f46c5134c9f0d33
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-28 20:43:12 -0600

    Update Constructors.py

--- Python/Constructors.py
+++ Python/Constructors.py
@@ -17,3 +17,13 @@ isabelles_ices = Store()
 
 alternative_rocks.store_name = "Alternative Rocks"
 isabelles_ices.store_name = "Isabelle's Ices"
+
+#---------------------------------------------------------
+
+can_we_count_it = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
+
+for element in can_we_count_it:
+  if hasattr(element, "count"):
+    print(str(type(element)) + " has the count attribute!")
+  else:
+    print(str(type(element)) + " does not have the count attribute :(")
