97bb596c8ff528cbe42e5e4a03adc41d04baa88d
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-28 19:32:04 -0600

    Update Read_CSV.py

--- Python/Read_CSV.py
+++ Python/Read_CSV.py
@@ -35,3 +35,15 @@ import json
 with open('message.json') as message_json:
   message = json.load(message_json)
 print(message['text'])
+
+#----------------------------------------------
+
+data_payload = [
+  {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
+   'follow up': 'But enough talk!'}
+]
+
+import json
+
+with open('data.json', 'w') as data_json:
+  json.dump(data_payload, data_json)
