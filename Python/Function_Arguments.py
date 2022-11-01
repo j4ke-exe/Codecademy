be562a0e2ab8eb2d593fac5c1e26bd7264988ed8
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-31 22:36:11 -0600

    Create Function_Arguments.py

---
+++ Python/Function_Arguments.py
@@ -0,0 +1,25 @@
+tables = {
+  1: ['Jiho', False],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+}
+print(tables)
+
+# Write your code below: 
+def assign_table(table_number, name, vip_status=False): 
+  tables[table_number] = [name, vip_status]
+
+assign_table(6, 'Yoni', False)
+print(tables)
+
+# Keyword arguments
+assign_table(table_number=3, name='Martha', vip_status=True)
+print(tables)
+
+# Positional table
+assign_table(4, 'Karla')
+print(tables)
