266fb8671c1b5965bbb015609fc1e1e05cad1837
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-02 11:14:41 -0600

    Create Function_call.py

---
+++ Python/Function_call.py
@@ -0,0 +1,8 @@
+def calculate_price_per_person(total, tip, split):
+  total_tip = total * (tip/100)
+  split_price = (total + total_tip) / split
+  print(split_price)
+
+# Write your code below: 
+table_7_total = [534.50, 20.0, 5]
+print(calculate_price_per_person(*table_7_total))
