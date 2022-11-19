2887e3cfb7ef178e3a9337de59aba36e66818bf3
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-19 09:45:43 -0600

    Update Enclosing_Namespace.py

--- Python/Enclosing_Namespace.py
+++ Python/Enclosing_Namespace.py
@@ -27,3 +27,21 @@ def calc_paint_amount(width, height):
 
 print('Number of paint gallons needed: ')
 print(str(calc_paint_amount(30,20)))
+
+#-------------------------------
+
+def print_available(color):
+  global paint_gallons_available 
+  paint_gallons_available = {
+    'red': 50,
+    'blue': 72,
+    'green': 99,
+    'yellow': 33
+  }
+
+  print('There are ' + str(paint_gallons_available[color]) + ' gallons available of ' + color + ' paint.')
+
+
+print_available('red')
+for color in paint_gallons_available:
+  print(color)
