6cb1003a952a621a573e8a59d7d02e2b883ad9e9
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-20 10:58:53 -0600

    Update Map_lambda.py

--- Python/Map_lambda.py
+++ Python/Map_lambda.py
@@ -3,3 +3,14 @@ int_list = [3, 6, 9]
 doubled = map(lambda input: input*2, int_list)
 
 print(list(doubled))
+
+#---------------------------------------------
+
+grade_list = [3.5, 3.7, 2.6, 95, 87]
+
+grades_100scale = map(lambda grade: grade*25 if grade <= 4.0 else grade, grade_list)
+
+# convert grades_100scale to a list
+updated_grade_list = list(grades_100scale)
+
+print(updated_grade_list)
