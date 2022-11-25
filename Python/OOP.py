fe49672b357e3ee8fc1a91adebdd96155fb500b1
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-25 13:14:37 -0600

    Update OOP.py

--- Python/OOP.py
+++ Python/OOP.py
@@ -34,3 +34,27 @@ e1 = Employee()
 e2 = Employee()
 e3 = Admin()
 e3.say_id()
+
+#-----------------------------------
+# Using super() inside of a sublclass to inherit specified methods from a parent class
+
+class Employee():
+  new_id = 1
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+
+  def say_id(self):
+    print("My id is {}.".format(self.id))
+
+class Admin(Employee):
+  def say_id(self):
+    # super() is used to call say_id() in order to print the Admin's given ID
+    super().say_id()
+    print("I am an admin.")
+
+e1 = Employee()
+e2 = Employee()
+e3 = Admin()
+e3.say_id()
+
