12777ae238026364a33893043a024cbabb79ba8c
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-25 16:16:56 -0600

    Create Properties.py

---
+++ Python/Properties.py
@@ -0,0 +1,18 @@
+class Box:
+ def __init__(self, weight):
+   self.__weight = weight
+ 
+ @property
+ def weight(self):
+   """Docstring for the 'weight' property"""
+   return self.__weight
+ 
+ 
+ @weight.setter
+ def weight(self, weight):
+   if weight >= 0:
+     self.__weight = weight
+ 
+ @weight.deleter
+ def weight(self):
+   del self.__weight
