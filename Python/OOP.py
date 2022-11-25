2fcbcf8b10445f9027b2b3fd4908656ce25c2cec
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-25 14:04:44 -0600

    Update OOP.py

--- Python/OOP.py
+++ Python/OOP.py
@@ -85,3 +85,29 @@ e2 = Employee()
 e3 = Admin()
 e4 = Manager()
 e4.say_id()
+
+#----------------------------------
+
+class Employee():
+  new_id = 1
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+
+  def say_id(self):
+    print("My id is {}.".format(self.id))
+
+class Admin(Employee):
+  def say_id(self):
+    super().say_id()
+    print("I am an admin.")
+
+class Manager(Admin):
+  def say_id(self):
+    super().say_id()
+    print("I am in charge!")
+
+# Write your code below
+meeting = [Employee(), Admin(), Manager()]
+for m in meeting:
+  m.say_id()
