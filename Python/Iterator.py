ff1bf9a0fdd5e5b2bc17be59f9c4b0b934f0d436
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-26 16:05:13 -0600

    Update Iterator.py

--- Python/Iterator.py
+++ Python/Iterator.py
@@ -8,7 +8,7 @@ class CustomerCounter:
     self.count += 1
     if self.count > 100:
       raise StopIteration
-    return self.count
+    return self
   
 customer_counter = CustomerCounter()
 for i in customer_counter:
