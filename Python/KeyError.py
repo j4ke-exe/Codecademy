18160456c7e8d76a22c212da98d6b1cf8b544f39
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-25 18:51:03 -0600

    Update KeyError.py

--- Python/KeyError.py
+++ Python/KeyError.py
@@ -127,3 +127,21 @@ def display_rewards_account(customer):
 
 customer = 'Mario'
 display_rewards_account(customer)
+
+#--------------------------------
+# Try, Except, Else, and Finally statements
+
+import database
+
+instrument = 'Kora'
+database.connect_to_database()
+
+try:
+  database.display_instrument_info(instrument)
+except KeyError:
+  print('Oh no! This instrument does not exist.')
+else:
+  print(instrument)
+# Write your code below: 
+finally:
+  database.disconnect_from_database()
