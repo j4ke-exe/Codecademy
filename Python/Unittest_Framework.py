6804eba1ec3860a0ac9c1e7aa079fe50a943636f
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-26 14:23:34 -0600

    Update Unittest_Framework.py

--- Python/Unittest_Framework.py
+++ Python/Unittest_Framework.py
@@ -146,3 +146,21 @@ class EntertainmentSystemTests(unittest.TestCase):
 
 
 unittest.main()
+
+#----------------------------
+# Expected Failures
+
+import unittest
+import feedback
+
+class CustomerFeedbackTests(unittest.TestCase):
+
+  # Write your code below:
+  @unittest.expectedFailure
+  def test_survey_form(self):
+    self.assertEqual(feedback.issue_survey(), 'Success')
+
+  def test_complaint_form(self):
+    self.assertEqual(feedback.log_customer_complaint(), 'Success')
+
+unittest.main()
