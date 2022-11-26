2d6a0827ed6285905d7bc5d48756801c6eb82309
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-26 12:32:12 -0600

    Update Testing.py

--- Python/Testing.py
+++ Python/Testing.py
@@ -9,3 +9,21 @@ flight_statuses = {
 print('***Small World Air Flight Information***')
 for flight, status in flight_statuses.items():
   print('Flight ' + str(flight) + ' status: ' + str(status))
+
+#--------------------------------
+
+destinations = {
+  'BUD': 'Budapest',
+  'CMN': 'Casablanca',
+  'IST': 'Istanbul'
+}
+print('Welcome to Small World Airlines!')
+print('What is the airport code of your travel destination?')
+destination = 'HND'
+
+
+# AssertionError handling
+assert destination in destinations, 'Sorry, Small World currently does not fly to this destination!'
+
+city_name = destinations[destination]
+print('Great! Retrieving information for your flight to ...' + city_name)
