93654309d39a0b2a3d8cbb586ceef35f209b4a52
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-26 13:34:54 -0600

    Update Unittest_Framework.py

--- Python/Unittest_Framework.py
+++ Python/Unittest_Framework.py
@@ -48,3 +48,19 @@ class EntertainmentSystemTests(unittest.TestCase):
     self.assertLess(device_temp, 35)
 
 unittest.main()
+
+#----------------------------
+# Exception and Warning Methods
+
+import unittest
+import alerts
+
+# Write your code here:
+class SystemAlertTests(unittest.TestCase):
+  def test_power_outage_alert(self):
+    self.assertRaises(alerts.PowerError, alerts.power_outage_detected, True)
+    
+  def test_water_levels_warning(self):
+  self.assertWarns(alerts.WaterLevelWarning, alerts.water_levels_check, 150)
+  
+unittest.main()
