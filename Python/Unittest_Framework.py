02eaf6a268d1cbd19a1eac9bb05a2524d316de60
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-26 13:47:27 -0600

    Update Unittest_Framework.py

--- Python/Unittest_Framework.py
+++ Python/Unittest_Framework.py
@@ -64,3 +64,22 @@ class SystemAlertTests(unittest.TestCase):
     self.assertWarns(alerts.WaterLevelWarning, alerts.water_levels_check, 150)
   
 unittest.main()
+
+#-----------------------------
+# Parameterizing Tests
+
+import unittest
+import entertainment
+
+class EntertainmentSystemTests(unittest.TestCase):
+
+  def test_movie_license(self):
+    daily_movies = entertainment.get_daily_movies()
+    licensed_movies = entertainment.get_licensed_movies()
+
+    for movie in daily_movies:
+      print(movie)
+      with self.subTest(movie):
+        self.assertIn(movie, licensed_movies)
+
+unittest.main()
