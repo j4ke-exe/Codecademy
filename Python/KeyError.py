c409060be3219e4ed233450b5bcbefd87dfe9be5
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-25 18:57:48 -0600

    Update KeyError.py

--- Python/KeyError.py
+++ Python/KeyError.py
@@ -145,3 +145,33 @@ else:
 # Write your code below: 
 finally:
   database.disconnect_from_database()
+
+#-------------------------------
+
+inventory = {
+  'Piano': 3,
+  'Lute': 1,
+  'Sitar': 2
+}
+
+
+#Write your code below (Checkpoint 2):
+class InventoryError(Exception):
+  pass
+
+
+def submit_order(instrument, quantity):
+  supply = inventory[instrument]
+  if quantity > supply:
+    raise InventoryError
+  else:
+  # Write your code below (Checkpoint 3 & 4): 
+    inventory[instrument] -= quantity
+    print('Successfully placed order! Remaining supply: ' + str(inventory[instrument]))
+
+instrument = 'Piano'
+quantity = 5
+submit_order(instrument, quantity)
+
+# Output: __main__.InventoryError
+# Reason is because requested order is 2 greater than the inventory size of 5
