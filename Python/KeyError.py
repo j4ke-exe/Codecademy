4d3d93b0a55733b8720ce83ca4f6e0d2e3726e1d
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-25 18:47:06 -0600

    Update KeyError.py

--- Python/KeyError.py
+++ Python/KeyError.py
@@ -108,3 +108,22 @@ except TypeError:
   print("Discount percentage must be a number!")
 except Exception:
   print("Hit an exception other than KeyError or TypeError!")
+
+#------------------------------
+
+customer_rewards = {
+  'Zoltan': 82570,
+  'Guadalupe': 29850,
+  'Mario': 17849
+}
+
+def display_rewards_account(customer):
+  try:
+    rewards_number = customer_rewards[customer]
+  except KeyError:
+    print("Customer was not found in rewards program!")
+  else:
+    print('Rewards account number is: ' + str(rewards_number))
+
+customer = 'Mario'
+display_rewards_account(customer)
