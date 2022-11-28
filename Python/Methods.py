19363aba2b01a4a66f1ceda4cc1b2cd3bb672bca
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-27 21:42:24 -0700

    Update Methods.py

--- Python/Methods.py
+++ Python/Methods.py
@@ -15,3 +15,17 @@ for i in student_id_generator:
     i = student_id_generator.send(25)
   print(next(student_id_generator))
   print(i)
+
+#---------------------------------
+# Methods: throw()
+
+def student_counter():
+  for i in range(1,5001):
+    yield i
+
+student_generator = student_counter()
+for student_id in student_generator:
+  # Write your code below:
+  if student_id > 100:
+    student_generator.throw(ValueError, "Invalid student ID")
+  print(student_id)
