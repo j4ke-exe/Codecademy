58d725151eda1d7ec6673b36edd8f08b6d2d3bb8
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-27 21:55:33 -0700

    Update Methods.py

--- Python/Methods.py
+++ Python/Methods.py
@@ -48,3 +48,24 @@ for item in my_generator:
   print(item)
   if item == 1:
     my_generator.close()
+
+#---------------------------------
+# Connecting Generators
+
+def science_students(x):
+  for i in range(1,x+1):
+    yield i
+
+def non_science_students(x,y):
+  for i in range(x,y+1):
+    yield i
+  
+# Write your code below
+def combined_students():
+  yield from science_students(5)
+  yield from non_science_students(10,15)
+  yield from non_science_students(25,30)
+
+student_generator = combined_students()
+for i in student_generator:
+  print(i)
