1f920cb3aa204c1c0a647ccdb2d816ed64eed61f
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-29 11:15:11 -0700

    Create Containers.py

---
+++ Python/Containers.py
@@ -0,0 +1,10 @@
+# Write your code below!
+company_name = "My Company"
+company_location = (2.5654, 1.6876)
+company_products = ["Shirt", "Pants", "Hat", "Socks", "Gloves"]
+company_data = {
+  "name": company_name, 
+  "location": company_location, 
+  "products": company_products
+  }
+print(company_data)
