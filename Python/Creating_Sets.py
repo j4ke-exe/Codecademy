7003c2a7529a24f72d4dd7be47864ffb354b860d
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-28 19:02:40 -0700

    Update Creating_Sets.py

--- Python/Creating_Sets.py
+++ Python/Creating_Sets.py
@@ -48,3 +48,24 @@ tag_set.remove('spam')
 
 song_data_users = {'Retro Words': tag_set}
 print(song_data_users)
+
+#----------------------------
+# Finding Elements in a Set
+
+allowed_tags = ['pop', 'hip-hop', 'rap', 'dance', 'electronic', 'latin', 'indie', 'alternative rock', 'classical', 'k-pop', 'country', 'rock', 'metal', 'jazz', 'exciting', 'sad', 'happy', 'upbeat', 'party', 'synth', 'rhythmic', 'emotional', 'relationship', 'warm', 'guitar', 'fiddle', 'romance', 'chill', 'swing']
+
+song_data_users = {'Retro Words': ['pop', 'explosion', 'hammer', 'bomb', 'warm', 'due', 'writer', 'happy', 'horrible', 'electric', 'mushroom', 'shed']}
+
+# Write your code below!
+tag_set = set(song_data_users['Retro Words'])
+
+bad_tags = []
+for tag in tag_set:
+  if tag not in allowed_tags:
+    bad_tags.append(tag)
+
+for tag in bad_tags:
+  tag_set.remove(tag)
+
+song_data_users = {'Retro Words': tag_set}
+print(song_data_users)
