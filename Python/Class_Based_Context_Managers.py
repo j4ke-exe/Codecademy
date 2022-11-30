001faa78d92f7d53e62d1688bf3bed4436a86bca
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-29 18:15:50 -0700

    Update Class_Based_Context_Managers.py

--- Python/Class_Based_Context_Managers.py
+++ Python/Class_Based_Context_Managers.py
@@ -35,3 +35,35 @@ class PoemFiles:
 
 with PoemFiles('poem.txt', 'w') as open_poem_file:
   open_poem_file.write('Hope is the thing with feathers')
+
+#----------------------------
+# Handling Exceptions I
+
+class PoemFiles:
+
+  def __init__(self, poem_file, mode):
+    print(' \n -- Starting up a poem context manager -- \n ')
+    self.file = poem_file
+    self.mode = mode
+
+  def __enter__(self):
+    print('Opening poem file')
+    self.opened_poem_file = open(self.file, self.mode)
+    return self.opened_poem_file
+
+  # Create your __exit__ method here:
+  def __exit__(self, exc_type, exc_value, traceback):
+    print(exc_type)
+    print(exc_value)
+    print(traceback)
+    return self.opened_poem_file.close()
+
+# First (Throws Error)
+# with PoemFiles('poem.txt', 'r') as file:
+#   print("---- Exception data below ----")
+#   print(file.uppercasewords())
+
+# Second (Handles Error)
+with PoemFiles('poem.txt', 'r') as file2:
+   print(file2.read())
+   print("---- Exception data below ----")
