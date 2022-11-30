ac07e5546350e1f6f8c4da934266169893f47aba
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-29 18:20:15 -0700

    Update Class_Based_Context_Managers.py

--- Python/Class_Based_Context_Managers.py
+++ Python/Class_Based_Context_Managers.py
@@ -65,3 +65,33 @@ class PoemFiles:
 with PoemFiles('poem.txt', 'r') as file2:
    print(file2.read())
    print("---- Exception data below ----")
+
+#---------------------------
+# Handling Exceptions II
+
+class PoemFiles:
+
+  def __init__(self, poem_file, mode):
+    print(' \n -- Starting up a poem context manager -- \n')
+    self.file = poem_file
+    self.mode = mode
+
+  def __enter__(self):
+    print(' \n --  Opening poem file -- \n')
+    self.opened_poem_file = open(self.file, self.mode)
+    return self.opened_poem_file
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    print(exc_type, exc_value, traceback, '\n')
+    # Write your code below: 
+    if isinstance(exc_value, AttributeError):
+      self.opened_poem_file.close()
+      return True
+
+with PoemFiles('poem.txt', 'r') as file:
+  print("---- Exception data below ---- \n ")
+  print(file.uppercasewords())
+
+with PoemFiles('poem.txt', 'r') as file2:
+  print(file2.read())
+  print(" \n ---- Exception data below ---- \n ")
