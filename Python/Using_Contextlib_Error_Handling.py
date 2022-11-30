ca68811145880227174ae2955be01d20e2fa8025
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-29 18:31:16 -0700

    Create Using_Contextlib_Error_Handling.py

---
+++ Python/Using_Contextlib_Error_Handling.py
@@ -0,0 +1,18 @@
+from contextlib import contextmanager
+ 
+@contextmanager
+def poem_files(file, mode):
+  print('Opening File')
+  open_poem_file = open(file, mode)
+  try:
+    yield open_poem_file
+  # Contextlib Error Handling Example: 
+  except AttributeError as e:
+    print(e)
+  finally:
+    print('Closing File')
+    open_poem_file.close()
+
+with poem_files('poem.txt', 'a') as opened_file:
+  print('Inside yield')
+  opened_file.sign('Buzz is big city. big city is buzz.')
