08cebc10e8fac6aa9c4b55d7b83871b07771959f
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-11-29 18:09:44 -0700

    Update Class_Based_Context_Managers.py

--- Python/Class_Based_Context_Managers.py
+++ Python/Class_Based_Context_Managers.py
@@ -12,3 +12,26 @@ class PoemFiles:
 
 with PoemFiles() as manager:
   print("Hope is the thing with feathers")
+
+#----------------------------
+# Class Based Context Managers II
+
+# Write your code below:
+class PoemFiles:
+
+  def __init__(self, poem_file, mode):
+    print("Starting up a poem context manager")
+    self.file = poem_file
+    self.mode = mode
+  
+  def __enter__(self):
+    print("Opening poem file")
+    self.opened_poem_file = open(self.file, self.mode)
+    return self.opened_poem_file
+  
+  def __exit__(self, *exc):
+    print("Closing poem file")
+    return self.opened_poem_file.close()
+
+with PoemFiles('poem.txt', 'w') as open_poem_file:
+  open_poem_file.write('Hope is the thing with feathers')
