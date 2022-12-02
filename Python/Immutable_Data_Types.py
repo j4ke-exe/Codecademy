ca29d75a4c3c8b5673bedea75b05822cbcbf04f4
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-02 09:42:20 -0700

    Update Immutable_Data_Types.py

--- Python/Immutable_Data_Types.py
+++ Python/Immutable_Data_Types.py
@@ -6,3 +6,10 @@ student = namedTuple("Student", ["name", "age", "grade"])
 scott = student("Scott", 28, "A")
 nicole = student("Nicole", 26, "B")
 john = student("John", 29, "D")
+
+print(scott.name) # Scott
+print(nicole.age) # 26
+print(john.grade) # D
+
+# Packaging student tuples into one
+students = (scott, nicole, john)
