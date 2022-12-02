bf1907d37c27f9a9995c7ed72f570e611c237c1e
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-02 10:16:36 -0700

    Update Lambda_Function.py

--- Python/Lambda_Function.py
+++ Python/Lambda_Function.py
@@ -30,3 +30,17 @@ def total_cost(ppsm, dim, area):
 
 print(total_cost(3, (5, 5), lambda b, h: b * h))         # Rectangular sheet costing 75 units
 print(total_cost(4, (6, 7), lambda b, h: 0.5 * (b * h))) # Rectangular sheet costing 84 units
+
+# Practice 1
+def odd_or_even(n, even_function, odd_function):
+  if n % 2 == 0:
+    return even_function(n)
+  else:
+    return odd_function(n)
+
+# Checkpoint 2 code goes here.
+square = lambda x: x * x
+cube = lambda x: x ** 3
+# Checkpoint 3 code goes here.
+test = odd_or_even(5, lambda x: cube, square)
+print(test)
