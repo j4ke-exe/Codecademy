4976d57f524ec164a29d44a5676c318da0ed8bf0
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-02 09:47:50 -0700

    Update Immutable_Data_Types.py

--- Python/Immutable_Data_Types.py
+++ Python/Immutable_Data_Types.py
@@ -13,3 +13,19 @@ print(john.grade) # D
 
 # Packaging student tuples into one
 students = (scott, nicole, john)
+
+# Exercise 1
+# Create a tuple for countries
+
+from collections import namedtuple
+
+# Checkpoint 1 code goes here.
+country = namedtuple("Country", ["name", "capital", "continent"])
+# Checkpoint 2 code goes here.
+france = country("France", "Paris", "Europe")
+japan = country("Japan", "Tokyo", "Asia")
+senegal = country("Senegal", "Dakar", "Africa")
+# Checkpoint 3 code goes here.
+countries = (france, japan, senegal)
+
+print(countries)
