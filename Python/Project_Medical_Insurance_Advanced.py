58d1b9fad71edd25a14e514664ad75dacea400ac
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-01-01 18:50:54 -0700

    Create Project_Medical_Insurance_Advanced.py

---
+++ Python/Project_Medical_Insurance_Advanced.py
@@ -0,0 +1,27 @@
+# Add your code here
+def analyze_smoker(smoker):
+  if smoker == 1:
+    print("To lower your cost, you should consider quitting smoking.\n")
+  else:
+    print("Smoking is not an issue for you.\n")
+
+def analyze_bmi(bmi_value):
+  if bmi_value > 30:
+    print("Your BMI is in the obese range. To lower your cost, you should significantly lower your BMI.\n")
+  elif bmi_value >= 25 and bmi_value <= 30:
+    print("Your BMI is in the overweight range. To lower your cost, you should lower your BMI.\n")
+  elif bmi_value >= 18.5 and bmi_value < 25:
+    print("Your BMI is in a healthy range.\n")
+  else:
+    print("Your BMI is in the underweight range. Increasing your BMI will not help lower your cost, but it will help improve your health.\n")
+
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.\n")
+  analyze_smoker(smoker)
+  analyze_bmi(bmi)
+  return estimated_cost
+ 
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, bmi = 26.2, num_of_children = 3, smoker = 1)
