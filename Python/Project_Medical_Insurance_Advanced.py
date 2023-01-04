b7da14f9299c2a3c10e7bdad2f441ddf2bbc5208
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-01-03 17:00:31 -0700

    Update Project_Medical_Insurance_Advanced.py

--- Python/Project_Medical_Insurance_Advanced.py
+++ Python/Project_Medical_Insurance_Advanced.py
@@ -54,3 +54,32 @@ estimated_insurance_data.append(("Maria", maria_insurance_cost))
 estimated_insurance_data.append(("Rohan", rohan_insurance_cost))
 estimated_insurance_data.append(("Valentina", valentina_insurance_cost))
 print(f'Here is the actual insurance cost data:', str(estimated_insurance_data))
+
+# -------------------------------
+
+names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel"]
+estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0]
+actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
+
+# Add your code here
+total_cost = 0
+for cost in actual_insurance_costs:
+  total_cost = cost
+
+average_cost = total_cost / len(actual_insurance_costs)
+
+print(f'Average Insurance Cost: {average_cost} dollars.\n')
+
+for i in range(len(names)):
+  name = names[i]
+  insurance_cost = actual_insurance_costs[i]
+  print(f'The insurance cost for {name} is {insurance_cost} dollars.\n')
+  if insurance_cost > average_cost:
+    print(f'The insurance cost for {name} is above average.\n')
+  elif insurance_cost < average_cost:
+    print(f'The insurance cost for {name} is below average.\n')
+  else:
+    print(f'The insurance cost for {name} is equal to the average.\n')
+
+updated_estimated_costs = [estimated_cost * 11/10 for estimated_cost in estimated_insurance_costs]
+print(updated_estimated_costs)
