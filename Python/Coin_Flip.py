9e5ef8eee4ceff789bb391f998fef85356566324
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-01-06 16:11:39 -0700

    Update Coin_Flip.py

--- Python/Coin_Flip.py
+++ Python/Coin_Flip.py
@@ -1,3 +1,14 @@
 import numpy
 
-print(numpy.random.binomial(n=1, p=0.5, size=1000))
+coin_flip = numpy.random.binomial(n=1, p=0.5, size=1000)
+
+tails = 0
+heads = 0
+for num in coin_flip:
+  if num == 0:
+    tails += 1
+  else:
+    heads += 1
+
+print(f'Number of tails: {tails}')
+print(f'Number of heads: {heads}')
