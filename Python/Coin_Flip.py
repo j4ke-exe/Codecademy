f4db95c3fbb0adfeff2c8c3bd703d6be2613af9a
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-01-06 16:22:29 -0700

    Update Coin_Flip.py

--- Python/Coin_Flip.py
+++ Python/Coin_Flip.py
@@ -1,14 +1,25 @@
 import numpy
+from typing import Tuple
 
-coin_flip = numpy.random.binomial(n=1, p=0.5, size=1000)
+def flip_coin(num_flips: int) -> Tuple[int, int]:
+    coin_flip = numpy.random.binomial(n=1, p=0.5, size=num_flips)
+    heads = sum(coin_flip)
+    tails = num_flips - heads
+    return heads, tails
 
-tails = 0
-heads = 0
-for num in coin_flip:
-  if num == 0:
-    tails += 1
-  else:
-    heads += 1
+def display_winner(heads: int, tails: int) -> str:
+    return "Heads" if heads > tails else "Tails"
 
-print(f'Number of tails: {tails}')
-print(f'Number of heads: {heads}')
+def main():
+    num_flips = 1000
+    heads, tails = flip_coin(num_flips)
+    print("Simple Heads or Tails Game")
+    print(f"A coin will flip {num_flips} times and tally up the total flips per side.")
+    print(f"The side with the most flips wins.")
+    print(f"Number of tails: {tails}")
+    print(f"Number of heads: {heads}")
+    print("-" * 20)
+    print(f"Winner: {display_winner(heads, tails)}")
+
+if __name__ == "__main__":
+    main()
